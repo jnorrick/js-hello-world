8fe8ae523e3a6fd3c7fdee178da4572e663a267a
Jessica Orrick <jessica.orrick@outlook.com>
2023-08-17 08:34:54 -0500

    now says Hello World

--- hello.js
+++ hello.js
@@ -4,8 +4,38 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello");
+console.log("Hello, world!");
 
+/* Change message printed */
+
+console.log("Welcome to LaunchCode!");
+
+/* Figure out what parentheses do. Will the code work without them? -no, without parentheses you get an error and it will not run.*/
+
+/* Remove quotation marks. Do you need both opening and closing? Can you use single vs double qoutes?  You can use single or double quotes for the string, will not run without those you get an error*/
+console.log ('Hello, world!');
+
+/* Remove the semi-colon: */
+console.log("Hello, world!")
+
+/* Print a number (or two numbers addeed together) */
+console.log( 5 + 3 );
+
+/* Print multiple messages one right after the other */
+console.log("Hello, world!");
+console.log("This is the second message.");
+console.log("Here is a third message, all in a row!");
+
+/* Two messages on the same line */
+console.log("Hello, world!\r Here is a second message on the same line."); 
+
+/* Message with quotation marks */
+
+console.log('Quoth the Raven "Nevermore".')
+
+/* Other: Concat two strings*/
+
+console.log("This is two strings " + "ADDED TOGETHER!")
 /*  
 Next, PLAY! 
 Modify your code on line 7 to try to accomplish the tasks listed below:
